c30198d9139218b512e3555e2a1ab32060fd58fc
290450 (Weronika Głuszczak) <01130577@pw.edu.pl>
2021-04-20 18:39:17 +0200

    init

--- AntiVanDerWaerden/AntiVanDerWaerden/Program.cs
+++ AntiVanDerWaerden/AntiVanDerWaerden/Program.cs
@@ -6,7 +6,28 @@ namespace AntiVanDerWaerden
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IStrategy strategy;
+
+            Console.WriteLine("Anty-van der Waerden");
+            Console.WriteLine();
+            Console.WriteLine("Podaj dane wejściowe: n k c");
+            var data = Console.ReadLine();
+            var splittedData = data.Split(' ');
+            Console.WriteLine("Wybierz tryb gry:");
+            Console.WriteLine("1) demo");
+            Console.WriteLine("2) testowy");
+            var tryb = Console.ReadLine();
+            if (tryb == "1" || tryb == "demo" || tryb == "1)")
+            {
+                Console.WriteLine("--------START--------");
+                for (int i = 1; i <= int.Parse(splittedData[0]); i++)
+                {
+                    Console.Write(i.ToString() + ' ');
+                }
+                Console.WriteLine();
+
+                strategy = new Strategy1(int.Parse(splittedData[0]), int.Parse(splittedData[1]), int.Parse(splittedData[2]), true);
+            }
         }
     }
 }
